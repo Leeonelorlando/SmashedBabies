e91df774cc9684687690409694f91df968d641fe
Leonel Orlando <leonelorlando98@hotmail.com>
2022-08-30 15:09:48 -0300

    Cambios

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -3,19 +3,19 @@ import logo from "./images/SMASHED BABIES LOGO 1.png";
 
 const Header = () => {
     return (
-        <div className="container">
+        <div className="container color_header">
             <ul className="nav d-flex align-items-center">
                 <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="# "><img src={logo} width="48" alt="Burguer King" /></a>
+                    <a className="link_header" aria-current="page" href="# "><img src={logo} width="100" alt="Smashed Babies" /></a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link link_header" href="# ">Pedí tu Smashed Burger</a>
+                    <a className="link_header" href="# ">Pedí tu Smashed Burger</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link link_header" href="# ">Menú</a>
+                    <a className="link_header" href="# ">Menú</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link link_header" href="# ">Cupones</a>
+                    <a className="link_header" href="# ">Cupones</a>
                 </li>
             </ul>
         </div>
