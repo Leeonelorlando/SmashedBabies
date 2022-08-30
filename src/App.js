420ad4389dc4c462d6beba241a923d6186e3965b
Leonel Orlando <leonelorlando98@hotmail.com>
2022-08-30 13:06:45 -0300

    Cambios

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Footer from './components/Footer';
 import Item from './components/Item';
 import Titulo from './components/Titulo';
 import Input from './components/Input';
+import Navbar from './components/NavBar';
+import BurguerButton from './components/Burgerbutton';
 
 function App() {
 
   return (
     <div className='container-fluid fondo_amarillo'>
     <Header />
+    <Navbar />
+    <BurguerButton />
     <hr />
     <Item />
     <Titulo />
